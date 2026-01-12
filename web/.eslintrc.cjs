68dc8774b37b0ec8f7362d33f8e68a73f65d97b1
Peptide90 <User94636@protonmail.com>
2026-01-12 11:16:27 +0000

    Add React Flow deps and lint/format tooling

---
+++ web/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2022: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint', 'import'],
+  settings: { react: { version: 'detect' } },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'import/order': [
+      'warn',
+      {
+        groups: [['builtin', 'external'], ['internal'], ['parent', 'sibling', 'index']],
+        'newlines-between': 'always',
+      },
+    ],
+  },
+};
--- web/README.md
+++ web/README.md
@@ -1,15 +1,18 @@
 # Substation Mimic Sim
+
 This project was started by Jordan Taylor (@TheElectricBrit) as an educational resource for creation high voltage single line mimic diagrams for use in teaching and media production.
 
 You can access the live version online here:
 
 Planned features include:
+
 - [ ] Drag and Drop component library
 - [ ] System for connecting components
 - [ ] Component state changing by clicking
 - [ ] Simple power flow simulation from source to load with respect to component state
 
 Advanced planned features:
+
 - [ ] Interlocking rules to have sequence of operation of switchgear
 - [ ] Tagging to allow group operation of equipment
 - [ ] Library of example mimics
